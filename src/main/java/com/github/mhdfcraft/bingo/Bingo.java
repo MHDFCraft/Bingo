76e8eba36e404452f311fc8d5534b9de776a1492
白神遥真可爱 <292200693@qq.com>
2024-09-08 19:34:44 +0800

    新建项目

---
+++ src/main/java/com/github/mhdfcraft/bingo/Bingo.java
@@ -0,0 +1,17 @@
+package com.github.mhdfcraft.bingo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bingo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
