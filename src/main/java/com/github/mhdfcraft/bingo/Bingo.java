972725ae57d6d1d35e3f533e962d05877093a4bd
白神遥真可爱 <292200693@qq.com>
2024-09-08 20:27:00 +0800

    修改插件名字

--- src/main/java/com/github/mhdfcraft/bingo/Bingo.java
+++ src/main/java/com/github/mhdfcraft/bingo/Bingo.java
@@ -10,15 +10,15 @@ public final class Bingo extends JavaPlugin {
     public void onEnable() {
         Util.instance = this;
 
-        LogUtil.colorLog("&f[MHDF-Tools] &a插件加载完成!");
-        LogUtil.colorLog("&f[MHDF-Tools] &a欢迎使用梦东系列插件 交流群号:129139830");
+        LogUtil.colorLog("&f[MHDF-Bingo] &a插件加载完成!");
+        LogUtil.colorLog("&f[MHDF-Bingo] &a欢迎使用梦东系列插件 交流群号:129139830");
     }
 
     @Override
     public void onDisable() {
         Util.instance = null;
 
-        LogUtil.colorLog("&f[MHDF-Tools] &9插件已卸载! 感谢您再次支持!");
-        LogUtil.colorLog("&f[MHDF-Tools] &a欢迎使用梦东系列插件 交流群号:129139830");
+        LogUtil.colorLog("&f[MHDF-Bingo] &9插件已卸载! 感谢您再次支持!");
+        LogUtil.colorLog("&f[MHDF-Bingo] &a欢迎使用梦东系列插件 交流群号:129139830");
     }
 }
